0880651d82c2cf36d51dafa7ffdd6f94f266392b
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-08-31 19:24:24 -0300

    Add aula string, int, float

---
+++ aula3atividade3.py
@@ -0,0 +1,15 @@
+# Capturar informações do usuário
+
+# Objetivo da atividade: Praticar os conceitos vistos até aqui.
+
+# Como: Faça um programa que capture o nome do usuário, altura em metros, idade e imprima
+
+
+nome = input("Digite seu nome: ")
+altura = float(input("Digite sua altura em metros: "))
+idade = int(input("Digite sua idade: "))
+
+# Imprimir as informações capturadas
+print("Nome:", nome)
+print("Altura:", altura, "metros")
+print("Idade:", idade, "anos")
