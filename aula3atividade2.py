dd40976bf88ddacafcdf7ec9b1067b5f6c21f37c
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-08-31 18:35:41 -0300

    Add aula3 joga junto

---
+++ aula3atividade2.py
@@ -0,0 +1,16 @@
+# Em SQUADs
+
+# Leiam o caso abaixo e executem usando Python.
+
+# A loja "ROUPAS SA" tem 2000 clientes e quer enviar mensagens nominais a cada um. A mensagem seria a seguinte:
+
+# Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10.
+
+cliente = "PAULA MARTINS"
+mes = "JANEIRO"
+cupom = "PAULAÉ10"
+valor = "R$500,00"
+desconto = "10%"
+
+print("Olá, " + cliente + ". Em " + mes + " você realizou uma compra no valor de " + valor +
+      " e ganhou um desconto de " + desconto + " em sua próxima compra. Use o cupom " + cupom + ".")
