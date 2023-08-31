71e72bd655861be684e0ed2028c7b49092862361
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-08-31 19:38:50 -0300

    Add atividade aula 3

--- aula3atividade3.py
+++ aula3atividade3.py
@@ -6,10 +6,16 @@
 
 
 nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
 altura = float(input("Digite sua altura em metros: "))
 idade = int(input("Digite sua idade: "))
 
 # Imprimir as informações capturadas
-print("Nome:", nome)
-print("Altura:", altura, "metros")
-print("Idade:", idade, "anos")
+# print("Nome:", nome)
+# print("Sobrenome:", sobrenome)
+# print("Altura:", altura, "metros")
+# print("Idade:", idade, "anos")
+
+
+print(
+    f" O nome é: {nome} {sobrenome} e a sua altura é {altura} metros e sua idade é {idade} sua altura é {altura} metros")
