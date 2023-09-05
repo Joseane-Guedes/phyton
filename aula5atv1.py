3663f9f3d7925043610d691277dca5a926958de3
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-05 18:33:55 -0300

    Add atividade aula5.1

---
+++ aula5atv1.py
@@ -0,0 +1,15 @@
+# TODO
+# INDIVIDUAL
+
+# PARTE 1
+
+# USANDO IF: Construa um script para verificar se o usuário tem uma idade maior que 18 anos, se tiver, imprima na tela "Indivíduo possui idade mínima para dirigir"
+
+# Solicite a idade do usuário
+idade = int(input("Digite sua idade: "))
+
+# Verifique se a idade é maior que 18
+if idade > 18:
+    print("Indivíduo possui idade mínima para dirigir")
+else:
+    print("Indivíduo não possui idade mínima para dirigir")
