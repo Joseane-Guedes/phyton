7b832462acebccdffe1a0365a5a306e4604590b4
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-05 18:54:32 -0300

    Feat: Adicionar condicao elif

--- aula5atv1.py
+++ aula5atv1.py
@@ -6,10 +6,16 @@
 # USANDO IF: Construa um script para verificar se o usuário tem uma idade maior que 18 anos, se tiver, imprima na tela "Indivíduo possui idade mínima para dirigir"
 
 # Solicite a idade do usuário
+
+# USANDO ELIF: Complemente o script feito, imprimindo na tela "Indivíduo tem entre 17 e 18 anos e ainda NÃO está apto para dirigir"
+
 idade = int(input("Digite sua idade: "))
 
-# Verifique se a idade é maior que 18
-if idade > 18:
+# Verifique a idade
+if idade >= 18:
     print("Indivíduo possui idade mínima para dirigir")
+# elif 17 <= idade <= 18:
+elif 17 <= idade and idade <= 18:
+    print("Indivíduo tem entre 17 e 18 anos e ainda NÃO está apto para dirigir")
 else:
     print("Indivíduo não possui idade mínima para dirigir")
