6336393b9c0e7f8ee991cd75af36d934dba0fb53
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-05 15:24:39 -0300

    Add colections

---
+++ colections.py
@@ -0,0 +1,23 @@
+# Lista
+frutas = ['Maracuja', 'Uva', 'Morango']
+print(30*'' + "Lista" + ''*30)
+print(frutas)
+
+# Tupla
+automoveis = ('Carro', 'Moto', 'Aviao')
+print(30*'' + "Tupla" + ''*30)
+print(automoveis)
+
+# Dicionario
+pessoa = {
+    "nome": "Renato",
+    "idade": 45,
+    "cidade": "Salvador"
+}
+print(30*'' + "Dicionário" + ''*30)
+print(pessoa)
+
+# Set
+cores = {'azul', 'amarelo', 'rosa'}
+print(30*'' + "Sets" + ''*30)
+print(cores)
