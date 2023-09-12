e76a6b1dfb49ebc7dbddf210ecb166928f1e9ded
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-12 19:51:56 -0300

    Add aula 8

--- aula8/aula8atv2.py
+++ aula8/aula8atv2.py
@@ -1,19 +1,21 @@
-import aula8atv1
-
 from aula8atv1 import verificar_paridade
 
 
 def verificar_paridade(matricula):
-    for numero in matricula:
-        if numero % 2 == 0:
-            print(f'O número {numero} é par.')
-        else:
-            print(f'O número {numero} é ímpar.')
+    if matricula % 2 == 0:
+        print(f'O número {matricula} é par.')
+    else:
+        print(f'O número {matricula} é ímpar.')
 
 
-matricula = []
-while len(matricula) < 5:
-    numero = int(input('Insira um número de matrícula: '))
-    matricula.append(numero)
+matriculas = []
 
-verificar_paridade(matricula)
+while len(matriculas) < 5:
+    matricula = int(input('Insira um número de matrícula: '))
+    matriculas.append(matricula)
+
+# Laço de repetição para verificar a paridade de cada número na lista
+for matricula in matriculas:
+    verificar_paridade(matricula)
+
+print(matriculas)
