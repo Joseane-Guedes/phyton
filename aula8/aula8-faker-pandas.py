f0419503e8f48a3d6b9c75b3f3dd87fede0490eb
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-14 16:51:27 -0300

    Add aula 8 panda-faker

---
+++ aula8/aula8-faker-pandas.py
@@ -0,0 +1,47 @@
+from faker import Faker
+import pandas as pd
+import random
+
+faker = Faker('pt_br')
+
+
+def remover_apelido(nome):
+
+    nome_fracionado = nome.split()
+    apelidos = ['Sra.', 'Sr.', 'Srta.']
+    for i in nome_fracionado:
+        if i in apelidos:
+            nome_fracionado.remove(i)
+    novo_nome = ' '.join(nome_fracionado)
+    return novo_nome
+
+
+def gerar_massa(quantidade):
+    personas = []
+
+    for i in range(quantidade):
+        data = {
+            "Nome": remover_apelido(faker.name()),
+            "cidade": faker.city(),
+            'bio': faker.text(),
+            # Gerar números aleatorios com o random
+            'idade': random.randint(18, 90)
+        }
+        personas.append(data)
+
+    df = pd.DataFrame(personas)
+
+    return df
+
+
+resultado = gerar_massa(120)
+
+
+# print(personas)
+
+resultado.to_csv("segunda_massa.csv", index=False)
+
+
+# print(df)
+
+# df.to_csv("primeira_massa.csv")
