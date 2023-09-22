d567dc67ee148c44496bd8a203ca79fa539131aa
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-22 16:15:59 -0300

    Add aula 6 de phyton

--- aula9/aulaatv6.py
+++ aula9/aulaatv6.py
@@ -1,27 +1,27 @@
-from faker import Faker
-import pandas as pd
+# from faker import Faker
+# import pandas as pd
 
-faker = Faker('pt_br')
-
-
-def gerar_massa(quantidade):
-    personas = []
-
-    for i in range(quantidade):
-        data = {
-            "Nome": faker.name(),
-            "Cidade": faker.city()
-        }
-        personas.append(data)
-
-    df = pd.DataFrame(personas)
-    return df
+# faker = Faker('pt_br')
 
 
-resultado = gerar_massa(120)
-print(resultado)
+# def gerar_massa(quantidade):
+#     personas = []
 
-resultado.to_csv("segundo_personas.csv")
+#     for i in range(quantidade):
+#         data = {
+#             "Nome": faker.name(),
+#             "Cidade": faker.city()
+#         }
+#         personas.append(data)
+
+#     df = pd.DataFrame(personas)
+#     return df
+
+
+# resultado = gerar_massa(120)
+# print(resultado)
+
+# resultado.to_csv("segundo_personas.csv")
 
 # from faker import Faker
 # import csv
@@ -44,3 +44,12 @@ resultado.to_csv("segundo_personas.csv")
 
 # # Criação e salvamento de personas em um arquivo CSV
 # create_and_save_personas('personas.csv')
+
+
+while True:
+    resposta = input("Pediu desculpas pela merda que fez? (S/N): ")
+    mensagem = "Teve humildade de assumir o erro, siga em frente!" if resposta.lower() == "s" else ("Não se arrependeu, volte para o início e tente novamente!" if resposta.lower()
+                                                                                                    == "n" else "Resposta inválida. Por favor, responda com 's' para 'sim' ou 'n' para 'não'.")
+    print(mensagem)
+    if resposta.lower() in ["s", "n"]:
+        break
