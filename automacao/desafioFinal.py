56e287b691730f90a3b135e99cd1b6154be93ea0
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-09-25 16:10:25 -0300

    Add desafio final - automatizar testes com phyton e selenium

---
+++ automacao/desafioFinal.py
@@ -0,0 +1,60 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from time import sleep
+import pyautogui
+import time
+import mouse
+
+
+def main():
+    navegador = Firefox()
+    navegador.get("https://web.whatsapp.com/")
+
+    input("Tecle ENTER após a leitura do QRCode!")
+    print("Automação em andamento! Favor aguardar!")
+
+    _switch_to_browser_tab()
+
+    grupo_nome = "Squad Orion"
+    pesquisar_contato(navegador, grupo_nome)
+
+    resultado_grupo = navegador.find_element(
+        By.XPATH, f"//span[@title='{grupo_nome}']")
+    resultado_grupo.click()
+
+    sleep(3)
+
+    mensagem = "Teste automatizado de envios de mensagens para o WhatsApp * Squad Orion * Renato, Katie, Ester e Josie!"
+
+    _send_message(mensagem)
+
+    sleep(3)
+
+    navegador.quit()
+
+
+def _switch_to_browser_tab():
+    pyautogui.keyDown('alt')
+    pyautogui.press('tab')
+    pyautogui.keyUp('alt')
+    sleep(3)
+
+
+def pesquisar_contato(navegador, nome_contato):
+    campo_pesquisa = navegador.find_element(
+        By.XPATH, "//div[@contenteditable='true']")
+    campo_pesquisa.send_keys(nome_contato)
+    sleep(3)
+
+
+def _send_message(mensagem):
+    mouse.move(690, 900, absolute=True, duration=0.1)
+    mouse.click('left')
+    time.sleep(1)
+
+    pyautogui.write(mensagem)
+    pyautogui.press('enter')
+
+
+if name == "main":
+    main()
